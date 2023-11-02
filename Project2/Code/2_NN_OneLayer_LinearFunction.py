4531bc38a96ccf74e700fc2e3e52d856eea8b2b0
EllenRekaa <ellenre@viken.no>
2023-11-02 12:54:00 +0100

    NN with  Linear function
     startet working on a linear function case

--- Project2/Code/2_NN_OneLayer_LinearFunction.py
+++ Project2/Code/2_NN_OneLayer_LinearFunction.py
@@ -1,41 +1,59 @@
 from sklearn.metrics import accuracy_score
-import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def loss(Y,ao):
+    return (1.0/n)*np.sum((Y - ao)**2)
 
-
-def SGD(X,y):
+#derivative of loss
+def d_L(Y,ao):
+    print(Y.shape, ao.shape)
+    return (2.0/n)*np.sum(Y - ao)
+    
+def SGD(X,y,lmbd, eta):
     for i in range(nbatch):
         random_index = M*np.random.randint(nbatch)
         xi = X[random_index:random_index+M]
         yi = y[random_index:random_index+M]
         dWo, dBo, dWh, dBh = backpropagation(xi, yi)
-
+        
         # regularization term gradients
         dWo += lmbd * Wo
         dWh += lmbd * Wh
-    
-            
-    return dWo, dBo, dWh, dBh
-    
 
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+        
+    return Wo, Bo, Wh, Bh
+
+def d_activ():
+    return 1    
+
+def activation(x):
+    #sigmoid
+    #f = 1/(1 + np.exp(-x))
+    
+    #Linear
+    f = x
+    
+    #2. order
+    #f= x**2
+
+    return f
 
 
 def feed_forward(X):
         
     # weighted sum of inputs to the hidden layer
-    zh = X.dot(Wh) + Bh
+    zh = X @ Wh + Bh
     # activation in the hidden layer
-    ah = sigmoid(zh)
+    ah = activation(zh)
 
     # weighted sum of inputs to the output layer
-    zo = ah.dot(Wo) + Bo
-    ao = sigmoid(zo)
-
+    zo = ah @ Wo + Bo
+    ao = activation(zo)
+    print(ao.shape)
     
     # for backpropagation need activations in hidden and output layers
     return ah, ao
@@ -44,12 +62,14 @@ def backpropagation(X, Y):
     ah, ao = feed_forward(X)  
 
     # error in the output layer
-    eo = ao - Y
-
+    eo = d_L(Y,ao)
+    print("Her",eo.shape)
     # error in the hidden layer
-    eh = eo @ Wo.T * ah * (1 - ah)
+    eh = eo @ Wo.T #d_a
+    
     
     # gradients for the output layer
+   #dWo = d_L() @ d_acitv() @ ah
     dWo = ah.T @ eo
     dBo = np.sum(eo, axis=0)
     
@@ -57,6 +77,7 @@ def backpropagation(X, Y):
     dWh = X.T @ eh
     dBh = np.sum(eh, axis=0)
     
+
     return dWo, dBo, dWh, dBh
 
 # we obtain a prediction by taking the class with the highest likelihood
@@ -67,37 +88,32 @@ def predict(X):
 
 # ensure the same random numbers appear every time
 np.random.seed(0)
-#e = 0.00001
-#l = 0.00001
-eta_vals = np.logspace(-5, -2, 14)
-lmbd_vals = np.logspace(-5, 1, 14)
+n = 5
+deg = 2
+eta_vals = np.logspace(-5, 1, 7)
+lmbd_vals = np.logspace(-5, 1, 7)
 epochs= 100
-lim = 0.0001
+lim = 0.001
+itter_limit = 10000
 
 M = 2   #size of each minibatch
 
-
 # data:
+x = 2*np.random.rand(n,1) 
+Y = 3*x + 4 #+ noise
+
 # Design matrix
-#X = np.array([ [0, 0], [0, 1], [1, 0],[1, 1], [0, 0], [0, 1], [1, 0],[1, 1]],dtype=np.float64)
-X = np.array([ [0, 0], [0, 1], [1, 0],[1, 1]],dtype=np.float64)
+poly = PolynomialFeatures(deg)
+X = poly.fit_transform(x)
+
 nbatch = int(len(X)/M) #number of minibatches
-# The XOR gate
-yXOR = np.array( [ 0, 1 ,1, 0])
-# The OR gate
-yOR = np.array( [ 0, 1 ,1, 1])
-# The AND gate
-yAND = np.array( [ 0, 0 ,0, 1])
-#yAND = np.array( [ 0, 0 ,0, 1,0, 0 ,0, 1])
-    
-y = yOR
-Y = np.reshape(y,(len(y),1))
+
 
 # Defining the neural network
-n_inputs, n_features = X.shape # (4,2) 4 sett med (x1,x2)
-n_hidden_neurons = 2 # 
-n_categories = 2 # 2 output (0,1)
-n_features = 2 # x1 og x2
+n_inputs, n_features = X.shape # # (10,3) 10 sett med (x0,x1,x2)
+n_hidden_neurons = 5 # 
+n_categories = 3 # 3 output (beta0,beta1,beta2)
+n_features = 3 # x0, x1 og x2
 
 
 # we make the weights normally distributed using numpy.random.randn
@@ -110,11 +126,6 @@ Wo = np.random.randn(n_hidden_neurons, n_categories)
 dWo = np.random.randn(n_hidden_neurons, n_categories)
 Bo = np.zeros(n_categories) + 0.01
 
-print(y)
-print(predict(X))
-
-
-
 test_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
 
 #loop over etas and lambdas
@@ -125,28 +136,23 @@ for i, eta in enumerate(eta_vals):
         while np.linalg.norm(dWo) > lim :
             epoch +=1
             # calculate gradient
-            dWo, dBo, dWh, dBh = SGD(X,Y)
-            
+            dWo, dBo, dWh, dBh = SGD(X,Y,lmbd,eta)
+            if epoch > itter_limit:
+                print("break SGD")
+                break
             # update weights and 
             Wo -= eta * dWo
             Bo -= eta * dBo
             Wh -= eta * dWh
-            Bh -= eta * dBh     
+            Bh -= eta * dBh
             
-            if epoch > 10000:
-                print("break SGD")
-                break
-            
-
-                        
+            #calculate gradient for whole dataset to use as a stop criteria 
+            dWo, dBo_t, dWh_t, dBh_t  = backpropagation(X, Y)
 
         if np.any(np.isnan(dWo)):
             print(eta, lmbd)
 
         test_accuracy[i][j] = accuracy_score(predict(X),Y)    
-        #if ((eta == e) and (lmbd == l)):
-        #    print(P)
-        #    print(predict(X))
         print(predict(X))
 
 
@@ -157,4 +163,3 @@ ax.set_title("Test Accuracy")
 ax.set_ylabel("$\eta$")
 ax.set_xlabel("$\lambda$")
 plt.show()
-
--- Project2/Code/NN_OneLayer_LinearFunction.py
+++ Project2/Code/NN_OneLayer_LinearFunction.py
@@ -1,25 +1,9 @@
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-
-def SGD(X,y):
-    for i in range(nbatch):
-        random_index = M*np.random.randint(nbatch)
-        xi = X[random_index:random_index+M]
-        yi = y[random_index:random_index+M]
-        dWo, dBo, dWh, dBh = backpropagation(xi, yi)
-
-        # regularization term gradients
-        dWo += lmbd * Wo
-        dWh += lmbd * Wh
-    
-            
-    return dWo, dBo, dWh, dBh
-    
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
@@ -47,57 +31,62 @@ def backpropagation(X, Y):
     eo = ao - Y
 
     # error in the hidden layer
-    eh = eo @ Wo.T * ah * (1 - ah)
+    eh = eo.dot(Wo.T) * ah #* (1 - ah)
+
     
     # gradients for the output layer
-    dWo = ah.T @ eo
+    dWo = ah.T.dot(eo)
     dBo = np.sum(eo, axis=0)
     
     # gradient for the hidden layer
-    dWh = X.T @ eh
+    dWh = X.T.dot(eh)
     dBh = np.sum(eh, axis=0)
     
-    return dWo, dBo, dWh, dBh
+    t = predict(X)
+
+    return t, ao, dWo, dBo, dWh, dBh
 
 # we obtain a prediction by taking the class with the highest likelihood
 def predict(X):
     ah, ao = feed_forward(X)
-    return np.argmax(ao, axis=1)
+    ypred = X.dot(ao.T)
+    diff= abs(ypred-Y)
+    minst = np.argmin(diff, axis=1)
+
+    print(minst)
+    print(ypred[:,4])
+    return ypred[:,4]
 
 
 # ensure the same random numbers appear every time
 np.random.seed(0)
-#e = 0.00001
-#l = 0.00001
-eta_vals = np.logspace(-5, -2, 14)
-lmbd_vals = np.logspace(-5, 1, 14)
-epochs= 100
-lim = 0.0001
-
-M = 2   #size of each minibatch
+e = 0.00001
+l = 0.00001
+eta_vals = np.logspace(-5, 1, 7)
+lmbd_vals = np.logspace(-5, 1, 7)
+epochs= 1000
 
 
 # data:
-# Design matrix
-#X = np.array([ [0, 0], [0, 1], [1, 0],[1, 1], [0, 0], [0, 1], [1, 0],[1, 1]],dtype=np.float64)
-X = np.array([ [0, 0], [0, 1], [1, 0],[1, 1]],dtype=np.float64)
-nbatch = int(len(X)/M) #number of minibatches
-# The XOR gate
-yXOR = np.array( [ 0, 1 ,1, 0])
-# The OR gate
-yOR = np.array( [ 0, 1 ,1, 1])
-# The AND gate
-yAND = np.array( [ 0, 0 ,0, 1])
-#yAND = np.array( [ 0, 0 ,0, 1,0, 0 ,0, 1])
+# Making meshgrid of datapoints and compute Franke's function
+#noise = 0.1*np.random.normal(0,1,10)
+#x = np.random.uniform(0, 1, 10)
+x = 2*np.random.rand(5,1) 
+y = 3*x + 4 #+ noise
+#y = np.sort(np.random.uniform(0, 1, 100))
+#z = FrankeFunction(x, y) + noise    
     
-y = yOR
-Y = np.reshape(y,(len(y),1))
+# Design matrix
+poly = PolynomialFeatures(2)
+X = poly.fit_transform(x)
+
+Y = y#np.reshape(y,(len(y),1))
 
 # Defining the neural network
-n_inputs, n_features = X.shape # (4,2) 4 sett med (x1,x2)
-n_hidden_neurons = 2 # 
-n_categories = 2 # 2 output (0,1)
-n_features = 2 # x1 og x2
+n_inputs, n_features = X.shape # (10,3) 10 sett med (x0,x1,x2)
+n_hidden_neurons = 5 # 
+n_categories = 3 # 3 output (beta0,beta1,beta2)
+n_features = 3 # x0, x1 og x2
 
 
 # we make the weights normally distributed using numpy.random.randn
@@ -107,43 +96,44 @@ Bh = np.zeros(n_hidden_neurons) + 0.01
 
 # weights and bias in the output layer
 Wo = np.random.randn(n_hidden_neurons, n_categories)
-dWo = np.random.randn(n_hidden_neurons, n_categories)
 Bo = np.zeros(n_categories) + 0.01
 
-print(y)
-print(predict(X))
+print("y-values ",Y)
+diff = predict(X)
+print("New accuracy on training data: " + str(accuracy_score(Y, predict(X))))
 
 
-
+#Loss = np.zeros(epochs)
 test_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
 
 #loop over etas and lambdas
 for i, eta in enumerate(eta_vals):
    for j, lmbd in enumerate(lmbd_vals):
         # loop over epochs
-        epoch = 0
-        while np.linalg.norm(dWo) > lim :
-            epoch +=1
-            # calculate gradient
-            dWo, dBo, dWh, dBh = SGD(X,Y)
+        for k in range(epochs):
+            # calculate gradents
+            t,P, dWo, dBo, dWh, dBh = backpropagation(X, Y)
+            #if np.any(np.isnan(dWo)):
+            #    print(eta, lmbd, k)
             
-            # update weights and 
+            # regularization term gradients
+            dWo += lmbd * Wo
+            dWh += lmbd * Wh
+            
+            # update weights and biases
             Wo -= eta * dWo
             Bo -= eta * dBo
             Wh -= eta * dWh
-            Bh -= eta * dBh     
-            
-            if epoch > 10000:
-                print("break SGD")
-                break
-            
-
-                        
-
+            Bh -= eta * dBh
+        
+            #t= predict(X)
+            #-(1.0 / target.size) * np.sum(target * np.log(X + 10e-10))
+            #Loss[k] = -1*np.sum( t *np.log(t +(1-Y_test)) * np.log(1-t))
+            #Loss[k] = -1*np.sum( t - np.log(1-t))
         if np.any(np.isnan(dWo)):
             print(eta, lmbd)
 
-        test_accuracy[i][j] = accuracy_score(predict(X),Y)    
+        test_accuracy[i][j] = accuracy_score(Y, t)#predict(X))    
         #if ((eta == e) and (lmbd == l)):
         #    print(P)
         #    print(predict(X))
